36752e47ee17c2753ef1fed9de0f92158c2cea10
jinli_regret <jinli.gjl@quadtalent.com>
2022-01-12 17:54:39 +0800

    chore: 移除包含敏感信息的记录

---
+++ db.ts
@@ -0,0 +1,34 @@
+import { MongoClient, MongoClientOptions } from 'mongodb';
+
+const mongodbUri = 'mongodb+srv://username:password@cluster0.l7efp.mongodb.net/node_server_demo?retryWrites=true&w=majority';
+const mongodbOptions = {
+  maxPoolSize: 20,
+  minPoolSize: 10,
+  connectTimeoutMS: 500,
+  useNewUrlParser: true
+} as MongoClientOptions;
+
+async function initPool() {
+  const client = new MongoClient(mongodbUri, mongodbOptions);
+  try {
+    await client.connect();
+    console.log(`worker ${process.pid} db connect success`);
+  } catch (error) {
+    client.close();
+    throw error;
+  }
+  return client;
+}
+
+function MongoPool() {}
+MongoPool.getInstance = (function() {
+  let instance: MongoClient;
+  return async function() {
+    if (!instance) {
+      instance = await initPool();
+    }
+    return instance;
+  }
+})();
+
+export default MongoPool;
