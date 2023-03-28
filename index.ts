562be476dd061e5366bfc9e531d97c650e8ea2c1
葛金力 <gejl@weipaitang.com>
2023-03-28 20:37:45 +0800

    feat: 接入 GPT API

--- index.ts
+++ index.ts
@@ -6,12 +6,15 @@ import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
 import cluster from 'cluster';
 import os from 'os';
+import * as dotenv from 'dotenv';
 
+// 加载环境变量
+dotenv.config();
 const numOfCPUs = os.cpus().length;
 
 // 使用 cluster 利用多核 cpu，并自动控制负载均衡
 if (cluster.isPrimary) {
-  console.log(`--- Hello koa, 主进程 ${process.pid} 正在运行`);
+  console.log(`主进程 ${process.pid} 正在运行`);
 
   for (let i = 0; i< numOfCPUs; i++) {
     // 创建子进程
@@ -35,7 +38,7 @@ if (cluster.isPrimary) {
   // 注册日志中间件
   app.use(logger());
 
-  app.listen(8001, () => {
+  app.listen(8100, () => {
     console.log(`工作进程 ${process.pid} 已启动`);
   });
 }
--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,20 +1,28 @@
 import Router from 'koa-router';
 import userRouter from './userRouter';
+import chatRouter from './chatRouter';
 
 const router = new Router();
 const viewRouter = new Router();
 
-viewRouter.get('/404', async (ctx) => {
-  await ctx.render('404', {
-    title: 'haha'
-  })
-})
-.get('/no_permission', async (ctx) => {
-  await ctx.render('noPermission')
-})
+viewRouter
+	.get('/404', async (ctx) => {
+		await ctx.render('404', {
+			title: 'haha',
+		});
+	})
+	.get('/no_permission', async (ctx) => {
+		await ctx.render('noPermission');
+	});
 
 // 注册所有 api 路由
-router.use('/api/v1', userRouter.routes(), userRouter.allowedMethods());
+router.use(
+	'/api/v1',
+	userRouter.routes(),
+	userRouter.allowedMethods(),
+	chatRouter.routes(),
+	chatRouter.allowedMethods()
+);
 
 // 注册页面路由
 router.use('/page', viewRouter.routes(), viewRouter.allowedMethods());
