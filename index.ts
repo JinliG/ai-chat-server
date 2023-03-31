cf9b09f1d8e58aeb28a7d67ce78fcb10743a5bc1
葛金力 <gejl@weipaitang.com>
2023-03-31 15:14:10 +0800

    feat: 增加 cors 中间件

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import views from 'koa-views';
 import path from 'path';
 import router from './server/routers';
 import bodyParser from 'koa-bodyparser';
+import cors from 'koa2-cors';
 import logger from 'koa-logger';
 import cluster from 'cluster';
 import os from 'os';
@@ -14,31 +15,39 @@ const numOfCPUs = os.cpus().length;
 
 // 使用 cluster 利用多核 cpu，并自动控制负载均衡
 if (cluster.isPrimary) {
-  console.log(`主进程 ${process.pid} 正在运行`);
+	console.log(`主进程 ${process.pid} 正在运行`);
 
-  for (let i = 0; i< numOfCPUs; i++) {
-    // 创建子进程
-    cluster.fork();
-  }
+	for (let i = 0; i < numOfCPUs; i++) {
+		// 创建子进程
+		cluster.fork();
+	}
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`--- 工作进程 ${worker.process.pid} 已退出`);
-  });
+	cluster.on('exit', (worker, code, signal) => {
+		console.log(`--- 工作进程 ${worker.process.pid} 已退出`);
+	});
 } else {
-  const app = new Koa();
+	const app = new Koa();
 
-  // 注册模板引擎
-  app.use(views(path.join(__dirname, './server/views'), {
-    extension: 'ejs'
-  }));
-  // 注册 ctx.body 解析中间件
-  app.use(bodyParser());
-  // 注册路由
-  app.use(router.routes()).use(router.allowedMethods())
-  // 注册日志中间件
-  app.use(logger());
+	/**
+	 * 使用 cors 设置跨域配置
+	 * 注意，cors 的注册要放在其他中间件前
+	 */
+	app.use(cors());
 
-  app.listen(8100, () => {
-    console.log(`工作进程 ${process.pid} 已启动`);
-  });
+	// 注册模板引擎
+	app.use(
+		views(path.join(__dirname, './server/views'), {
+			extension: 'ejs',
+		})
+	);
+	// 注册 ctx.body 解析中间件
+	app.use(bodyParser());
+	// 注册路由
+	app.use(router.routes()).use(router.allowedMethods());
+	// 注册日志中间件
+	app.use(logger());
+
+	app.listen(8100, () => {
+		console.log(`工作进程 ${process.pid} 已启动`);
+	});
 }
--- server/models/aiChatModel.ts
+++ server/models/aiChatModel.ts
@@ -27,11 +27,8 @@ function createOpenAIModel() {
 					{
 						model: 'text-davinci-003',
 						prompt,
-						temperature: 0.5,
-						max_tokens: 256,
-						top_p: 1,
-						frequency_penalty: 0,
-						presence_penalty: 0,
+						temperature: 1,
+						max_tokens: 2048,
 					},
 					{
 						httpsAgent: tunnel.httpsOverHttp({
